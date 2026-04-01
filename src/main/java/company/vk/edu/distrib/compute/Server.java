720a785b0f59c1939b39db060e95306449c327c2
AntonDor <dorogovanton291@gmail.com>
2026-04-01 20:42:16 +0300

    fix: вернул чужой код

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,16 +1,15 @@
 package company.vk.edu.distrib.compute;
 
-import company.vk.edu.distrib.compute.aldor7705.KVServiceFactorySimple;
+import module java.base;
+import company.vk.edu.distrib.compute.mandesero.KVServiceFactoryImpl;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
 
 public class Server {
 
     void main() throws IOException {
         var log = LoggerFactory.getLogger("server");
         var port = 8080;
-        KVService storage = new KVServiceFactorySimple().create(port);
+        KVService storage = new KVServiceFactoryImpl().create(port);
         storage.start();
         log.info("Server started on port {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
