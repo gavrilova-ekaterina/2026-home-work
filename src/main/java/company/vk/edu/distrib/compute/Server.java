8224f41552ffc9413766b6084aa4ccbb1ed8d736
Igor Popov <ip-adsense@yandex.ru>
2026-04-06 08:21:46 +0000

    revert Server.java to orig

--- src/main/java/company/vk/edu/distrib/compute/Server.java
+++ src/main/java/company/vk/edu/distrib/compute/Server.java
@@ -1,9 +1,7 @@
 package company.vk.edu.distrib.compute;
 
 import module java.base;
-
-import company.vk.edu.distrib.compute.ip.PopovIgorKVServiceFactoryImpl;
-
+import company.vk.edu.distrib.compute.dummy.DummyKVServiceFactory;
 import org.slf4j.LoggerFactory;
 
 public class Server {
@@ -11,7 +9,7 @@ public class Server {
     void main() throws IOException {
         var log = LoggerFactory.getLogger("server");
         var port = 8080;
-        KVService storage = new PopovIgorKVServiceFactoryImpl().create(port);
+        KVService storage = new DummyKVServiceFactory().create(port);
         storage.start();
         log.info("Server started on port {}", port);
         Runtime.getRuntime().addShutdownHook(new Thread(storage::stop));
